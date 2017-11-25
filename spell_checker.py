8988f7129a3ec8faf18dc7dad4259f2271eff638
LevyvoNet <eladlevy2@gmail.com>
2017-11-25 19:15:14 +0200

    succeed correcting a word!

--- spell_checker.py
+++ spell_checker.py
@@ -1,9 +1,9 @@
 import itertools
-import functools
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from numpy import log
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+EMPTY_CHAR = '~'
 
 
 class Edit(namedtuple('Edit', ['error', 'type'], verbose=False)):
@@ -58,6 +58,62 @@ def learn_language_model(files, n=3, lm=None):
     return ngrams
 
 
+def get_string_count(word_count):
+    """Return count of strings at the size of 1,2 in word_count
+    
+    We are going to use this in order to create the error distribution (this is the denominator).
+
+    Args:
+        word_count(dict): A dictionary of words and their counts.
+
+    Returns:
+        dict. the matrix of strings at the size of 1,2 and their counts in the given dict.
+            for example: {'ab': 5, 'x': 1...}
+    """
+    ret = {}
+    for word in word_count:
+        for i in range(len(word) - 2):
+            ret[word[i] + word[i + 1]] = ret.get(word[i] + word[i + 1], 0) + word_count[word]
+            ret[word[i]] = ret.get(word[i], 0) + word_count[word]
+            # for the last character of word.
+
+        ret[word[-1]] = ret.get(word[-1], 0) + word_count[word]
+
+    ret[EMPTY_CHAR] = len(word_count)
+    return ret
+
+
+def get_error_count(errors_file):
+    """Return the count of errors occurred in the given file.
+
+    The structure is the same as the confusion matrix representation. Means {err_type: dict} err_type
+    is in ['deletion', 'insertion', 'substitution', 'transposition'] and dict is {(corr,err): count}
+    """
+    # Smooth the error count using laplace correction.
+    error_count = {'deletion': {(x + y, x): 1 for x, y in itertools.permutations(ALPHABET, 2)},
+                   'insertion': {(x, x + y): 1 for x, y in itertools.permutations(ALPHABET, 2)},
+                   'substitution': {(x, y): 1 for x, y in itertools.permutations(ALPHABET, 2)},
+                   'transposition': {(x + y, y + x): 1 for x, y in itertools.permutations(ALPHABET, 2)}
+                   }
+    error_count['deletion'].update({(x, EMPTY_CHAR): 1 for x in ALPHABET})
+    error_count['deletion'].update({(x + x, x): 1 for x in ALPHABET})
+    error_count['insertion'].update({(EMPTY_CHAR, x): 1 for x in ALPHABET})
+    error_count['insertion'].update({(x, x + x): 1 for x in ALPHABET})
+
+    with open(errors_file, 'r') as misspellings:
+        for line in misspellings:
+            # cut \n
+            [misspelled_words, real_word] = line.lower()[:-1].split('->')
+            misspelled_words = misspelled_words.split(', ')
+            for misspelled_word in misspelled_words:
+                _, edits = optimal_string_alignment(real_word, misspelled_word)
+            errors = [edit for edit in edits if edit.error[0] != edit.error[1]]
+            for err in errors:
+                error_count[err.type][err.error] = error_count[err.type].get(err.error, 0) + 1
+
+    return error_count
+
+
 def create_error_distribution(errors_file, lexicon):
     """ Returns a dictionary {str:dict} where str is in:
     <'deletion', 'insertion', 'transposition', 'substitution'> and the inner dict {tuple: float} represents the confution matrix of the specific errors
@@ -78,69 +134,70 @@ def create_error_distribution(errors_file, lexicon):
         A dictionary of error distributions by error type (dict).
 
     """
-    with open(errors_file, 'r') as erros:
-        for err in erros:
-            [misspelled_word, real_word] = err.split('->')
-            print 'misspelled:{}, real: {}'.format(misspelled_word, real_word)
+    # initialize the error model with a laplace smoothing.
+    error_distribution = {'deletion': dict(),
+                          'insertion': dict(),
+                          'substitution': dict(),
+                          'transposition': dict()}
+
+    errors_count = get_error_count(errors_file)
+    string_count = get_string_count(lexicon)
+
+    for err_type, type_count in errors_count.iteritems():
+        for err, err_count in type_count.iteritems():
+            original_string = err[0]
+            # Here we are using laplace smoothing for string appearances.
+            error_distribution[err_type][err] = \
+                float(err_count) / (string_count.get(original_string, 0) + 1)
+
+    return error_distribution
 
 
-def generate_text(lm, m=15, w=None):
-    """ Returns a text of the specified length, generated according to the
-     specified language model using the specified word (if given) as an anchor.
-
-     Args:
-        lm (dict): language model used to generate the text.
-        m (int): length (num of words) of the text to generate (default 15).
-        w (str): a word to start the text with (default None)
-
-    Returns:
-        A sequrnce of generated tokens, separated by white spaces (str)
-    """
-
-
-def optimal_string_alignment(w, s):
+def optimal_string_alignment(src, dst):
     """Calculate OSA distance between two strings and the edit series from one to the other.
 
     Args:
-        w(string): a word.
-        s(string): another word.
+        src(string): a word.
+        dst(string): another word.
 
     Returns:
         Distance. (price, edits) while price is the edit distance between the two
             strings (number) and edits is a list contains Edit represents the
             edits from w to s.
     """
-    # def nice_print_d(d):
-    #     two_dim_arr = [[d[(i, j)] for i in range(len(w) + 1)] for j in range(len(s) + 1)]
-    #     for i in range(1, len(two_dim_arr)):
-    #         print two_dim_arr[i][1:]
+
+    def nice_print_d(d):
+        two_dim_arr = [[d[(i, j)] for i in range(len(src))] for j in range(len(dst))]
+        for i in range(1, len(two_dim_arr)):
+            print two_dim_arr[i][1:]
 
     # arbitrary value to initialize the dynamic programming matrix, no cell should be left with this value
     # in the end.
     init_val = -100
+    src, dst = EMPTY_CHAR + src, EMPTY_CHAR + dst
 
     # Initialize a dictionary which represents the dynamic programming matrix.
     d = {(i, j): Distance(init_val, [])
-         for i in range(-1, len(w) + 1)
-         for j in range(-1, len(s) + 1)}
+         for i in range(len(src))
+         for j in range(len(dst))}
 
     # Initialize the first column of d with deletions.
-    for i in range(len(w) + 1):
-        edit = d[i - 1, 0].edits + [Edit((w[i - 1], '-'), 'deletion')] if i != 0 else []
+    for i in range(len(src)):
+        edit = d[i - 1, 0].edits + [Edit((src[i], EMPTY_CHAR), 'deletion')] if i != 0 else []
         d[i, 0] = Distance(i, edit)
 
     # Initialize the first row of d with insertions.
-    for j in range(len(s) + 1):
-        edit = d[0, j - 1].edits + [Edit(('-', s[j - 1]), 'insertion')] if j != 0 else []
+    for j in range(len(dst)):
+        edit = d[0, j - 1].edits + [Edit((EMPTY_CHAR, dst[j]), 'insertion')] if j != 0 else []
         d[0, j] = Distance(j, edit)
 
-    for i in range(len(w) + 1)[1:]:
-        for j in range(len(s) + 1)[1:]:
-            substitution_cost = 0 if w[i - 1] == s[j - 1] else 1
+    for i in range(len(src))[1:]:
+        for j in range(len(dst))[1:]:
+            substitution_cost = 0 if src[i] == dst[j] else 1
 
-            deletion = Edit((w[i - 1], '-'), 'deletion')
-            insertion = Edit(('-', s[j - 1]), 'insertion')
-            substitution = Edit((w[i - 1], s[j - 1]), 'substitution')
+            deletion = Edit((src[i - 1] + src[i], src[i - 1]), 'deletion')
+            insertion = Edit((dst[j - 1], dst[j - 1] + dst[j]), 'insertion')
+            substitution = Edit((src[i], dst[j]), 'substitution')
 
             possible_edits = {
                 deletion: Distance(d[i - 1, j].price + 1,
@@ -151,114 +208,39 @@ def optimal_string_alignment(w, s):
                                        d[i - 1, j - 1].edits + [substitution])
             }
 
-            if i > 1 and j > 1 and w[i - 1] == s[j - 2] and w[i - 2] == s[j - 1]:
-                transposition = Edit((w[i - 2] + w[i - 1], s[j - 2] + s[j - 1]), 'transposition')
+            if i > 1 and j > 1 and src[i] == dst[j - 1] and src[i - 1] == dst[j]:
+                transposition = Edit((src[i - 1] + src[i], dst[j - 1] + dst[j]), 'transposition')
                 possible_edits[transposition] = Distance(d[i - 2, j - 2].price + 1,
                                                          d[i - 2, j - 2].edits + [transposition])
 
             best_edit = min(possible_edits.iterkeys(), key=lambda e: possible_edits[e].price)
             d[i, j] = possible_edits[best_edit]
 
-    return d[(len(w), len(s))]
+    return d[(len(src) - 1, len(dst) - 1)]
 
 
-# def damerau_levenshtein_and_edits(w, s):
-#     """Calculate Damerau-Levenshtein distance between two strings edit series from one to the other.
-#
-#     Args:
-#         w(string): a word.
-#         s(string): another word.
-#
-#     Returns:
-#         tuple. (distance, edits) while distance is the edit distance between the two
-#             strings (number) and edits is a list contains Edit represents the
-#             edits from s to w.
-#     """
-#
-#     def nice_print_d(d):
-#         two_dim_arr = [[d[(i, j)] for i in range(len(w) + 1)] for j in range(len(s) + 1)]
-#         # two_dim_arr = [[i - 1 for i in range(len(w) + 1)]] + two_dim_arr
-#         # for i in range(len(s) + 1):
-#         #     two_dim_arr[i] = [i - 1] + two_dim_arr[i]
-#
-#         for i in range(1, len(two_dim_arr)):
-#             print two_dim_arr[i][1:]
-#
-#     init_val = -100
-#     max_dist = len(w) + len(s)
-#     da = {x: 0 for x in ALPHABET}
-#
-#     # initialize the dynamic programming matrix.
-#     d = {(i, j): Distance(init_val, [])
-#          for i in range(-1, len(w) + 1)
-#          for j in range(-1, len(s) + 1)}
-#
-#     d[(-1, -1)] = Distance(max_dist, [])
-#
-#     for i in range(len(w) + 1):
-#         d[(i, -1)] = Distance(max_dist, [])
-#         edit = d[i - 1, 0].edits + [Edit(('-', w[i - 1]), 'deletion')] if i != 0 else []
-#         d[(i, 0)] = Distance(i, edit)
-#
-#     for j in range(len(s) + 1):
-#         d[(-1, j)] = Distance(max_dist, [])
-#         edit = d[0, j - 1].edits + [Edit((s[j - 1], '-'), 'insertion')] if j != 0 else []
-#         d[(0, j)] = Distance(j, edit)
-#
-#     for i in range(len(w) + 1)[1:]:
-#         db = 0
-#         for j in range(len(s) + 1)[1:]:
-#             k = da[s[j - 1]]
-#             l = db
-#             if w[i - 1] == s[j - 1]:
-#                 cost = 0
-#                 db = j
-#             else:
-#                 cost = 1
-#
-#             possible_edits = {
-#                 Edit(('-', w[i - 1]), 'deletion'): Distance(d[i - 1, j].price + 1,
-#                                                             d[i - 1, j].edits + [Edit(('-', w[i - 1]), 'deletion')]),
-#                 Edit((s[j - 1], '-'), 'insertion'): Distance(d[i, j - 1].price + 1,
-#                                                              d[i, j - 1].edits + [Edit((s[j - 1], '-'), 'insertion')]),
-#                 Edit((s[j - 1], w[i - 1]), 'substitution'): Distance(d[i - 1, j - 1].price + cost,
-#                                                                      d[i - 1, j - 1].edits + [
-#                                                                          Edit((s[j - 1], w[i - 1]), 'substitution')]),
-#                 Edit((s[j - 2] + s[j - 1], w[i - 2] + w[i - 1]), 'transposition'):
-#                     Distance(d[k - 1, l - 1].price + (i - k - 1) + 1 + (j - l - 1),
-#                              d[i - 2, j - 2].edits + [
-#                                  Edit((s[j - 2] + s[j - 1], w[i - 2] + w[i - 1]),
-#                                       'transposition')])
-#             }
-#             best_edit = min(possible_edits.iterkeys(), key=lambda e: possible_edits[e].price)
-#             d[(i, j)] = possible_edits[best_edit]
-#
-#             da[w[i - 1]] = i
-#
-#     nice_print_d(d)
-#     return d[(len(w), len(s))]
-
-
-def generate_candidates(w, d):
+def generate_candidates(misspelled_word, lexicon):
     """Generate candidates for a word correction and their matching edits.
 
     Args:
-        w(string): the misspelled word.
-        d(list): dictionary contains all of the words.
+        misspelled_word(string): the misspelled word.
+        lexicon(list): list contains all of the words.
 
     Returns:
         tuple. (candidates, edits) tuples contains the
             candidate words (list) and their edits from given misspelled word w
             (list of tuples represents edits).
     """
-    candidates, edits = [], []
-    for can in d:
-        edit_distance, edits = optimal_string_alignment(can, w)
+    candidates, errors = [], []
+    for can in lexicon:
+        edit_distance, edits = optimal_string_alignment(can, misspelled_word)
         if edit_distance <= 2:
             candidates.append(can)
-            edits.append(edits)
+            # avoid appending "empty" substitutions (e.g ('a', 'a'))
+            real_errors = [edit for edit in edits if edit.error[0] != edit.error[1]]
+            errors.append(real_errors)
 
-    return candidates, edits
+    return candidates, errors
 
 
 def correct_word(w, word_counts, errors_dist):
@@ -286,14 +268,30 @@ def correct_word(w, word_counts, errors_dist):
     def channel_multi_edits(edits):
         return reduce(lambda x, y: x * y, [channel(edit) for edit in edits], 1)
 
-    def candidate_prob(can, edits):
+    def candidate_score(can, edits):
         return log(prior(can)) + log(channel_multi_edits(edits))
 
-    can_probs = {
-        can: candidate_prob(can, edits)
-        for can, edits in itertools.izip(generate_candidates(w, word_counts.keys()))
+    candidates_score = {
+        can: candidate_score(can, edits)
+        for can, edits in itertools.izip(*generate_candidates(w, word_counts.iterkeys()))
     }
-    return max(can_probs.iterkeys(), key=lambda c: can_probs[c])
+    print candidates_score
+
+    return max(candidates_score.iterkeys(), key=lambda c: candidates_score[c])
+
+
+def generate_text(lm, m=15, w=None):
+    """ Returns a text of the specified length, generated according to the
+     specified language model using the specified word (if given) as an anchor.
+
+     Args:
+        lm (dict): language model used to generate the text.
+        m (int): length (num of words) of the text to generate (default 15).
+        w (str): a word to start the text with (default None)
+
+    Returns:
+        A sequrnce of generated tokens, separated by white spaces (str)
+    """
 
 
 def correct_sentence(s, lm, err_dist, c=2, alpha=0.95):
